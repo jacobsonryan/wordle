c291c942e2cedde620963a0455991ffac10a4bc3
Ryan Jacobson <ryanjacobson@ryans-mbp.myfiosgateway.com>
2022-04-14 20:40:01 -0400

    invalid word check

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
 	const [input, setInput] = useState('')
 	const [word, setWord] = useState([])
 	const [guesses, setGuesses] = useState([])
+	const [valid, setValid] = useState(true)
 
 	useEffect(() => {
 		setWord(Words.valid[Math.floor(Math.random() * Words.valid.length)])
@@ -32,8 +33,8 @@ function App() {
 		let remainingLetters = word
 		for(let i = 0; i < 5; i++) {
 			if(inputArr[i].letter === word[i]) {
-				inputArr[i].color = '#538E4E'
 				remainingLetters = remainingLetters.replace(inputArr[i].letter, '')
+				inputArr[i].color = '#538E4E'
 			} else {
 				inputArr[i].color = '#3a3a3c'
 			}			
@@ -52,18 +53,27 @@ function App() {
 		}
 	}
 
+	function handleChange(e) {
+		setInput(e.target.value)
+		if(e.target.value.length === 5 && !Words.valid.includes(e.target.value)) {
+			setValid(false)
+		} else {
+			setValid(true)
+		}
+	}
+
   return (
 		<>
 			<h1>Wordle Clone</h1>
 			<form onSubmit={(e) => submitGuess(e)}>
-				<input spellCheck='false' type='text' maxLength='5' onChange={(e) => setInput(e.target.value)} value={input}  autoFocus={true} onBlur={({ target }) => target.focus()}/>
+				<input spellCheck='false' type='text' maxLength='5' onChange={(e) => handleChange(e)} value={input}  autoFocus={true} onBlur={({ target }) => target.focus()}/>
 			</form>
 			<div className='guesses'>
 				{guesses.map((word, index) => {
 					return <p key={index} className='animate' style={{border: '2px solid ' + word.color, backgroundColor: word.color, animationDelay: (index % 5) * 0.10 + 's'}}>{word.letter}</p>
 				})}
 				{[...input].map((letter, index) => {
-					return <p key={index} className='shaking' style={{border: '2px solid #565758'}}>{letter}</p>
+					return <p key={index} className='shaking' style={{border: valid ? '2px solid #565758' : '2px solid #a82d2d', backgroundColor: valid ? '#121213': '#a82d2d'}}>{letter}</p>
 				})}
 				<div className='empty'>
 					<p style={{border: '2px solid #3a3a3c'}}></p>
